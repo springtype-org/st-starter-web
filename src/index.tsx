5c21bbf5238aa6e664ef034ac1e920ca82dcfbfb
kyr0 <info@aron-homberg.de>
2019-10-29 12:24:43 +0100

    chore: fixed API

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import { st } from "springtype/core";
 import { component } from "springtype/web/component";
+import { ILifecycle } from "springtype/web/component/interface";
 import { tsx } from "springtype/web/vdom";
 import "./polyfills";
 
 @component()
-export class IndexPage extends st.component {
+export class IndexPage extends st.component implements ILifecycle {
   render() {
     return <p>Hello, world!</p>;
   }
